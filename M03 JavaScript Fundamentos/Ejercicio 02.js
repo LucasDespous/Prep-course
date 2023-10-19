228858591bb9903608922172ad5e20b75944e475
Lucas Despous <lucasdespous@gmail.com>
2023-10-19 18:12:24 -0300

    commit de prueba

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,34 +4,64 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   var devolver = "Henry"
+   return devolver
 }
+var resultado1 = devolverString("string")
+console.log(resultado1)
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var resultado2 = x + y;
+   return resultado2;
 }
+var operacion2 = suma(10, 40);
+console.log(operacion2);
+
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var resultado3 = x - y;
+   return resultado3;
 }
+var operacion3 = resta(20, 10);
+console.log(operacion3);
+
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var resultado4 = x / y;
+   return resultado4;
 }
+var operacion4 = divide(50, 5);
+console.log(operacion4);
+
+
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var resultado5 = x * y;
+   return resultado5;
 }
+var operacion5 = multiplica(5, 4);
+console.log(operacion5);
+
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var resultado6 = x % y;
+   return resultado6;
 }
+var operacion6 = obtenerResto(30, 3);
+console.log(operacion6);
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,32 +6,59 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   var operacion1 = num ** 2;
+   return operacion1;
 }
+var resultado1 = elevarAlCuadrado(4);
+console.log(resultado1);
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   var operacion2 = num ** 3;
+   return operacion2;
 }
+var resultado2 = elevarAlCubo(3);
+console.log(resultado2);
+
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   var operacion3 = num ** exponent;
+   return operacion3;
 }
+var resultado3 = elevar(4, 4);
+console.log(resultado3);
+
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   var operacion4 = Math.round(num);
+   return operacion4
 }
+var resultado4 = redondearNumero(4.50);
+console.log(resultado4);
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   var operacion5 = Math.ceil(num);
+   return operacion5;
 }
+var resultado5 = redondearHaciaArriba(2.30);
+console.log(resultado5);
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   var operacion6 = Math.random();
+   return operacion6;
 }
+var resultado6 = numeroRandom();
+console.log(resultado6);
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
